d3f70ea51a0cdfd58dd27906c27a06819d1cf7cd
supravi-bidchat <supravi.singbal@bidchat.com>
2019-03-08 16:47:27 +0530

    finished

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,15 +3,24 @@ import styles from './Burger.module.css';
 import BurgerIngredient from '../Burger/BurgerIngredient/BurgerIngredient';
 
 const burger = ( props ) => {
+
+    const transformedIngredient = Object.keys(props.ingredients)
+        .map(igKey=>{
+            console.log([...Array(props.ingredients[igKey])]);
+            
+            return [...Array(props.ingredients[igKey])].map((_,i)=>{
+                return <BurgerIngredient key={igKey+i} type={igKey}/>
+            });
+        });
+        
     return(
         <div className={ styles.Burger }>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredient}
             <BurgerIngredient type="bread-bottom"/>
 
         </div>
     );
-}
+};
 
 export default burger;
