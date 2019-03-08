d92b00d5280a4ac5c150751b36b428944beb24f8
supravi-bidchat <supravi.singbal@bidchat.com>
2019-03-08 12:10:21 +0530

    finished

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import styles from './Layout.module.css';
 
 const layout = ( props ) => (
     <Aux>
         <div>Toolbar , Sidebar , Backdrop</div>
-        <main>
+        <main className={ styles.Content }>
             {props.children}
         </main>
     </Aux>
